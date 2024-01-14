1cc9b6993b49fa0bc6f108dc4cc97ab429c14061
yaj55billy <yaj55ushydna@gmail.com>
2024-01-14 21:23:21 +0800

    deploy set

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/vue-2023-week6/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
